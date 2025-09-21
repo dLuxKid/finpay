407e9db59d5fd971f2f05a1606cae5af29beb951
dLuxKid <adetunjimarvellous09@gmail.com>
2025-09-21 15:55:05 +0100

    Add UserRole type and role field to User model

--- internal/models/users.model.go
+++ internal/models/users.model.go
@@ -6,12 +6,22 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type UserRole string
+
+const (
+	ADMIN    UserRole = "ADMIN"
+	USER     UserRole = "USER"
+	BUSINESS UserRole = "BUSINESS"
+)
+
 type User struct {
-	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Username  string             `bson:"username" json:"username"`
-	Email     string             `bson:"email" json:"email"`
-	Password  string             `bson:"password" json:"-"`
-	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
-	DeletedAt *time.Time         `bson:"deleted_at,omitempty" json:"-"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Username string             `bson:"username" json:"username"`
+	Email    string             `bson:"email" json:"email"`
+	Password string             `bson:"password" json:"-"`
+	Role     UserRole           `bson:"role" json:"role"`
+
+	CreatedAt time.Time  `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time  `bson:"updated_at" json:"updated_at"`
+	DeletedAt *time.Time `bson:"deleted_at,omitempty" json:"-"`
 }
